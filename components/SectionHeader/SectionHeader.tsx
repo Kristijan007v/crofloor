41d9b510fc39bea3028b7873b4168e8cb7cbf6ea
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-04-29 01:35:10 +0200

    Added ImageWithFallback component

--- components/SectionHeader/SectionHeader.tsx
+++ components/SectionHeader/SectionHeader.tsx
@@ -1,22 +1,28 @@
-import React from "react";
 import Image from "next/image";
+import React from "react";
+import ImageWithFallback from "../ImageWithFallback/ImageWithFallback";
 
 interface Props {
   title: string;
   image: string;
   alt: string;
+  key?: number;
 }
 
-export default function SectionHeader({ title, image, alt }: Props) {
+export default function SectionHeader({ title, image, alt, key }: Props) {
   return (
     <div className="relative h-48 w-full">
-      <Image
+      <ImageWithFallback
+        key={key}
         src={`/images/${image}`}
+        fallbackSrc={`/images/image-error.jpg`}
         alt={alt}
         layout="fill"
         objectFit="cover"
         className="opacity-85"
+        priority={true}
       />
+
       <div className="relative h-full w-full bg-black/40">
         <h1 className="h1__white absolute bottom-0 p-4">{title}</h1>
       </div>
