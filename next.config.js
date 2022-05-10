ef415b3f8d44136154d432cef72c16a71d8a4c71
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-05-10 15:59:17 +0200

    Testing bundle analyzer to remove unused JS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
 const withPWA = require("next-pwa");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const MEDIA_DOMAIN = process.env.MEDIA_DOMAIN;
 
@@ -9,15 +12,16 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-  i18n,
-  reactStrictMode: true,
-  images: {
-    domains: ["localhost", MEDIA_DOMAIN],
-  },
-});
+(module.exports = [withBundleAnalyzer]),
+  withPWA({
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+    },
+    i18n,
+    reactStrictMode: true,
+    images: {
+      domains: ["localhost", MEDIA_DOMAIN],
+    },
+  });
