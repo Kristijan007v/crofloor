4e3e21d007aa485edf803164ebbb5fd9af8c5cb3
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-05-18 01:40:25 +0200

    Reverted fix on PageHeader

--- components/PageHeader/PageHeader.tsx
+++ components/PageHeader/PageHeader.tsx
@@ -27,7 +27,7 @@ export default function PageHeader({
 
   return (
     <>
-      <div>
+      <div className="relative h-screen w-full">
         <Image
           src={`${backgroundImage}`}
           alt={alt}
@@ -38,42 +38,40 @@ export default function PageHeader({
         />
         <div className="relative flex h-screen w-full flex-col bg-black/40">
           <Navigation />
-          <div className="flex flex-grow items-center justify-center">
-            <div className="flex flex-col items-center justify-center space-y-6  p-6">
-              <h1 className="h1 uppercase text-white">
-                {title}
-                {/* <span className="text-gray-800"> RICCO</span> */}
-              </h1>
-              <div className="relative h-36 w-4/5">
-                <Image
-                  src={`${featuredImage}`}
-                  alt={alt}
-                  layout="fill"
-                  objectFit="cover"
-                  className="opacity-85 rounded-md"
-                  priority={true}
-                />
-              </div>
-              <div className="absolute bottom-0 left-0 p-4">
-                <Tag
-                  text={`${lang === "en" ? "Gallery" : "Galerija"}`}
-                  onclick={() => {
-                    window.location.href = `#${galleryAnchor}`;
-                  }}
-                />
-              </div>
-              <div className="absolute bottom-0 right-0 p-4">
-                <Tag
-                  text={`${lang === "en" ? "Details" : "Detalji"}`}
-                  onclick={() => {
-                    window.location.href = `#${detailsAnchor}`;
-                  }}
-                />
-              </div>
-              <p className="paragraph rounded-md bg-primary-bg p-4">
-                {description}
-              </p>
+          <div className="flex flex-col items-center justify-center space-y-6  p-6">
+            <h1 className="h1 uppercase text-white">
+              {title}
+              {/* <span className="text-gray-800"> RICCO</span> */}
+            </h1>
+            <div className="relative h-36 w-4/5">
+              <Image
+                src={`${featuredImage}`}
+                alt={alt}
+                layout="fill"
+                objectFit="cover"
+                className="opacity-85 rounded-md"
+                priority={true}
+              />
             </div>
+            <div className="absolute bottom-0 left-0 p-4">
+              <Tag
+                text={`${lang === "en" ? "Gallery" : "Galerija"}`}
+                onclick={() => {
+                  window.location.href = `#${galleryAnchor}`;
+                }}
+              />
+            </div>
+            <div className="absolute bottom-0 right-0 p-4">
+              <Tag
+                text={`${lang === "en" ? "Details" : "Detalji"}`}
+                onclick={() => {
+                  window.location.href = `#${detailsAnchor}`;
+                }}
+              />
+            </div>
+            <p className="paragraph rounded-md bg-primary-bg p-4">
+              {description}
+            </p>
           </div>
         </div>
       </div>
