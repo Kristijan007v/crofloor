8dc40b46c06530091897310fdca7e97552d8ed22
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-05-24 21:43:55 +0200

    Improved font sizing on 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -27,12 +27,14 @@ export default function Custom404() {
     <Skeleton title="404" navigation={true}>
       <div className="m-6 flex h-screen flex-col items-center justify-center space-y-4 text-center">
         <p>
-          <span className="text-8xl font-extrabold">4</span>
-          <span className="text-8xl font-extrabold text-yellow-special">0</span>
-          <span className="text-8xl font-extrabold">4</span>
+          <span className="text-8xl font-extrabold lg:text-9xl">4</span>
+          <span className="text-8xl font-extrabold text-yellow-special lg:text-9xl">
+            0
+          </span>
+          <span className="text-8xl  font-extrabold lg:text-9xl">4</span>
         </p>
-        <p className="h2">{t("title")}</p>
-        <p className="paragraph">{t("description")}</p>
+        <p className="h2 h2__responsive">{t("title")}</p>
+        <p className="paragraph p__responsive">{t("description")}</p>
         <ButtonDefault
           text={t("button")}
           ariaLabel={t("button")}
