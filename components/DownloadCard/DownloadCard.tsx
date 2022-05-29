92995cd94f3b6a37dd1bb54a770832cb149ed535
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-05-29 18:33:03 +0200

    Improved about page layout

--- components/DownloadCard/DownloadCard.tsx
+++ components/DownloadCard/DownloadCard.tsx
@@ -9,16 +9,17 @@ interface Props {
 
 export default function DownloadCard({ text, downloadURL }: Props) {
   return (
-    <div className="flex items-center justify-between rounded-xl bg-primary-bg p-3">
+    <div
+      className="flex items-center justify-between rounded-xl bg-primary-bg p-3 hover:cursor-pointer"
+      onClick={() => {
+        window.location.href = `${downloadURL}`;
+      }}
+    >
       <div className="flex items-center space-x-4">
         <FileIcon />
         <p>{text}</p>
       </div>
-      <DownloadIcon
-        onclick={() => {
-          window.location.href = `${downloadURL}`;
-        }}
-      />
+      <DownloadIcon />
     </div>
   );
 }
