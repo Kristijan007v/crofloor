a7de0bdfe7691e19a90b4eb2f0b1c07d0c578da6
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-05-31 15:21:18 +0200

    Improved FAQ styling and layout

--- components/Faq/Faq.tsx
+++ components/Faq/Faq.tsx
@@ -16,18 +16,24 @@ export default function Faq({ question, answer }: Props) {
 
   return (
     <div
-      className={`ontap-remove space-y-2" flex max-h-48 cursor-pointer flex-col`}
+      className={`ontap-remove space-y-2" flex max-h-48 cursor-pointer flex-col `}
     >
       <div
         className="flex items-center justify-between"
         onClick={() => toogle()}
       >
-        <p className="p__responsive">{question}</p>
+        <p
+          className={`p__responsive ${
+            isOpen ? "font-semibold" : "font-normal"
+          }`}
+        >
+          {question}
+        </p>
         {isOpen ? <ArrowUp /> : <ArrowDown />}
       </div>
 
       {isOpen && (
-        <div className="overflow-y-auto rounded-2xl bg-primary-gray p-4">
+        <div className="overflow-y-auto rounded-md bg-primary-gray p-4">
           <div className="">
             <p className="faq__responsive">{answer}</p>
           </div>
